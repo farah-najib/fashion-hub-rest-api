9c271843c48454a103a997eafaf0988181b9ca00
Farah Najib <farahengnajib@gmail.com>
2025-04-12 00:38:37 +0200

    add server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const productRoutes = require('./routes/productRoutes')
+const userRoutes = require('./routes/userRoutes')
+const orderRoutes = require('./routes/orderRoutes')
+
+dotenv.config()
+connectDB()
+
+const app = express()
+app.use(express.json())
+
+// Routes
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+
+const path = require('path')
+
+// Serve static files (like images)
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
