daf5b2ced299a09f3888d057fe190b3ce9644f00
Farah Najib <farahengnajib@gmail.com>
2025-04-12 00:38:03 +0200

    add API endpoints

---
+++ routes/products.js
@@ -0,0 +1,13 @@
+// routes/products.js
+const express = require('express')
+const router = express.Router()
+const productController = require('../controllers/productController')
+
+// Routes for CRUD operations
+router.post('/', productController.createProduct)
+router.get('/', productController.getProducts)
+router.get('/:id', productController.getProduct)
+router.put('/:id', productController.updateProduct)
+router.delete('/:id', productController.deleteProduct)
+
+module.exports = router
