ecb21bc51caf3827490ffa0db8f652f2c5b5624f
Farah Najib <farahengnajib@gmail.com>
2025-04-12 00:59:10 +0200

    log

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const connectDB = async () => {
+    console.log(process.env.MONGODB_URI)
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
