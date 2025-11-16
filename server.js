c346a01124e5eb95939624fee1e6e06507c20daa
farah-najib <farahengnajib@gmail.com>
2025-11-16 11:45:01 +0100

    update bugs

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const productRoutes = require('./routes/productRoutes')
 const userRoutes = require('./routes/userRoutes')
 const orderRoutes = require('./routes/orderRoutes')
 const { swaggerUi, swaggerDocument } = require('./docs/swagger')
+const CSS_URL =
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
+
 dotenv.config()
 connectDB()
 
@@ -13,15 +16,19 @@ const app = express()
 app.use(express.json())
 
 app.use(
-    cors({
-        origin: ['https://app-fashion-hub.vercel.app', 'http://localhost:5173'], // Allow frontend to communicate with the backend
-        methods: ['GET', 'POST'],
-        allowedHeaders: ['Content-Type', 'Authorization']
-    })
+  cors({
+    origin: ['https://app-fashion-hub.vercel.app', 'http://localhost:5173'], // Allow frontend to communicate with the backend
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  })
 )
 // Routes
 // Swagger UI
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(
+  '/docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL })
+)
 
 app.use('/api/user', userRoutes)
 app.use('/api/products', productRoutes)
@@ -30,10 +37,12 @@ app.use('/api/orders', orderRoutes)
 const path = require('path')
 
 // Serve static files (like images)
-app.use('/images', express.static(path.join(__dirname, 'public/images/products')))
-
+app.use(
+  '/images',
+  express.static(path.join(__dirname, 'public/images/products'))
+)
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
