5e7fde848d08360cbe492fa7ac84e18dee6b6137
farah-najib <farahengnajib@gmail.com>
2025-11-16 11:18:42 +0100

    fix bug

--- docs/swagger.js
+++ docs/swagger.js
@@ -1,8 +1,12 @@
 const swaggerUi = require('swagger-ui-express')
-const YAML = require('yamljs')
+// const YAML = require('yamljs')
+const YAML = require('yaml')
 const path = require('path')
+const fs = require('fs')
 
-const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'))
+// const swaggerDocument = YAML.load(path.join(__dirname, 'openapi.yaml'))
+const file = fs.readFileSync(path.join(__dirname, 'openapi.yaml'), 'utf8')
+const swaggerDocument = YAML.parse(file)
 
 module.exports = {
   swaggerUi,
